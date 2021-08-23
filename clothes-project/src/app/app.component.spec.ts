143e3d62aa4a244d650810eb92d9da826cc158b4
silviu4444 <pascariu.octavian98@gmail.com>
2021-08-23 09:35:55 +0300

    removed some useless features

--- clothes-project/src/app/app.component.spec.ts
+++ clothes-project/src/app/app.component.spec.ts
@@ -20,10 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clothes-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('clothes-project');
-  });
 
 });
