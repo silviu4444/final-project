9544d5473384625f1e01c816777b0c97eccf3248
silviu4444 <pascariu.octavian98@gmail.com>
2021-08-31 14:56:51 +0300

    environment production:  true

--- clothes-project/src/environments/environment.prod.ts
+++ clothes-project/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseAPIKey: 'AIzaSyCFzrG03M-I4Wo-atIfLtdVMdM2XwjsUNw',
   loginUrl:
     'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
