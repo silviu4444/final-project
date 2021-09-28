f5621a40af97bcf574576e4bc6d1eb67f1db036c
silviu4444 <pascariu.octavian98@gmail.com>
2021-09-28 16:04:59 +0300

    raiting component

--- clothes-project/src/app/shared/shared.module.ts
+++ clothes-project/src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { LoadingSpinnerComponent } from "./components/loading-spinner/loading-spinner.component";
+import { StarsRaitingComponent } from './components/stars-raiting/stars-raiting.component';
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent],
-  exports: [LoadingSpinnerComponent]
+  declarations: [LoadingSpinnerComponent, StarsRaitingComponent],
+  exports: [LoadingSpinnerComponent, StarsRaitingComponent]
 })
 
 export class SharedModule {}
