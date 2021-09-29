0c746538ff5bdc34ad8a53131d3551c3429d1d10
silviu4444 <pascariu.octavian98@gmail.com>
2021-09-29 15:58:39 +0300

    added cart state

--- clothes-project/src/app/shared/interfaces/interfaces.ts
+++ clothes-project/src/app/shared/interfaces/interfaces.ts
@@ -1,3 +1,6 @@
+import { Laptop } from "src/app/modules/home/pages/home/models/laptop.model";
+import { MobilePhone } from "src/app/modules/home/pages/home/models/phone.model";
+
 interface RAM {
   ramOptions: string[];
   itemIdx: number;
@@ -11,4 +14,11 @@ interface Storage {
 export interface PhoneMemory {
   ram: RAM;
   storage: Storage;
+}
+
+export interface CartItem {
+  memorySelected?: string;
+  storageSelected?: string;
+  colorSelected: string;
+  item: MobilePhone | Laptop;
 }
